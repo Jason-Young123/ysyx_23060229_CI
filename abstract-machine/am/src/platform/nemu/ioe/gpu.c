8f61ec83e9c762cb5f30ecb2cf737330b9cf5fd0
tracer-ysyx <tracer@ysyx.org>
2024-04-02 22:15:08 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:15:08 up  5:45,  1 user,  load average: 0.23, 0.07, 0.02

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -9,15 +9,28 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = 400, .height = 300,
     .vmemsz = 0
   };
 }
 
+
+//ctl 有如下成员变量：int x, y, void* pixels, int w,h, bool sync
+//其中x，y是画方块的起始位置（左上角坐标），pixel为方块的颜色，w和h为方块的尺寸，sync
+//表示这个方块是否要被立刻显示
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+  	uint32_t* color_buf = (uint32_t*)ctl -> pixels;
+	for(int x = 0; x < ctl -> w; x++){
+		for(int y = 0; y < ctl -> h; y++){
+			outl(FB_ADDR + (y + ctl -> y)*400 + (x + ctl -> x), color_buf[y*ctl->w+x]);
+		}
+	}
+		
+	if (ctl->sync)
+    	outl(SYNC_ADDR, 1);
+	else
+		outl(SYNC_ADDR, 0);
+
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
