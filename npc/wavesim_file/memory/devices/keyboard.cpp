36fcdcea1e2a1e2078cd546f38e408eee6880b0c
tracer-ysyx <tracer@ysyx.org>
2024-04-05 14:47:07 +0800

    > wavesim in npc ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:07 up  4:14,  1 user,  load average: 0.18, 0.26, 0.20

--- npc/wavesim_file/memory/devices/keyboard.cpp
+++ npc/wavesim_file/memory/devices/keyboard.cpp
@@ -1,6 +1,36 @@
 #include <memory/memory.h>
 
 
+/*#define NEMU_KEYS(f) \
+  f(ESCAPE) f(F1) f(F2) f(F3) f(F4) f(F5) f(F6) f(F7) f(F8) f(F9) f(F10) f(F11) f(F12) \
+f(GRAVE) f(1) f(2) f(3) f(4) f(5) f(6) f(7) f(8) f(9) f(0) f(MINUS) f(EQUALS) f(BACKSPACE) \
+f(TAB) f(Q) f(W) f(E) f(R) f(T) f(Y) f(U) f(I) f(O) f(P) f(LEFTBRACKET) f(RIGHTBRACKET) f(BACKSLASH) \
+f(CAPSLOCK) f(A) f(S) f(D) f(F) f(G) f(H) f(J) f(K) f(L) f(SEMICOLON) f(APOSTROPHE) f(RETURN) \
+f(LSHIFT) f(Z) f(X) f(C) f(V) f(B) f(N) f(M) f(COMMA) f(PERIOD) f(SLASH) f(RSHIFT) \
+f(LCTRL) f(APPLICATION) f(LALT) f(SPACE) f(RALT) f(RCTRL) \
+f(UP) f(DOWN) f(LEFT) f(RIGHT) f(INSERT) f(DELETE) f(HOME) f(END) f(PAGEUP) f(PAGEDOWN)
+
+#define NEMU_KEY_NAME(k) NEMU_KEY_ ## k,
+
+enum {
+  NEMU_KEY_NONE = 0,
+  MAP(NEMU_KEYS, NEMU_KEY_NAME)
+  //展成NEMU_KEY_ESCAPE = 1,
+  //NEMU_KEY_F1 = 2,
+  //...
+};
+
+#define SDL_KEYMAP(k) keymap[SDL_SCANCODE_ ## k] = NEMU_KEY_ ## k;
+static uint32_t keymap[256] = {};
+
+static void init_keymap() {
+  MAP(NEMU_KEYS, SDL_KEYMAP)
+  //SDL_KEYMAP(ESCAPE) ->   keymap[SDL_SCANCODE_ESCAPE] = NEMU_KEY_ESCAPE
+  //                        keymap[SDL_SCANCODE_F1] = NEMU_KEY_F1
+}
+*/
+
+
 extern bool difftest_to_skip;
 
 uint32_t keyboard_addr[1] = {0};
