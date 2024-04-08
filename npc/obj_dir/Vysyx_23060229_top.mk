70a5bca7dc0f003ff1dc195a282bddeb7ac71974
tracer-ysyx <tracer@ysyx.org>
2024-04-08 17:27:45 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:27:45 up 6 min,  1 user,  load average: 0.97, 1.52, 0.97

--- npc/obj_dir/Vysyx_23060229_top.mk
+++ npc/obj_dir/Vysyx_23060229_top.mk
@@ -4,7 +4,7 @@
 # Execute this makefile from the object directory:
 #    make -f Vysyx_23060229_top.mk
 
-default: Vysyx_23060229_top__ALL.a
+default: Vysyx_23060229_top
 
 ### Constants...
 # Perl executable (from $PERL)
@@ -35,15 +35,37 @@ VM_PREFIX = Vysyx_23060229_top
 VM_MODPREFIX = Vysyx_23060229_top
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
+	-I/home/jason/Desktop/ysyx-workbench/npc/wavesim_file \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
+	/lib/x86_64-linux-gnu/libreadline.so \
+	/lib/x86_64-linux-gnu/libSDL2.so \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
+	difftest \
+	engine \
+	gpu \
+	keyboard \
+	serial \
+	timer \
+	memory \
+	register \
+	expr \
+	sdb \
+	watchpoint \
+	wavesim \
 
 # User .cpp directories (from .cpp's on Verilator command line)
 VM_USER_DIR = \
+	/home/jason/Desktop/ysyx-workbench/npc/wavesim_file \
+	/home/jason/Desktop/ysyx-workbench/npc/wavesim_file/difftest \
+	/home/jason/Desktop/ysyx-workbench/npc/wavesim_file/engine \
+	/home/jason/Desktop/ysyx-workbench/npc/wavesim_file/memory \
+	/home/jason/Desktop/ysyx-workbench/npc/wavesim_file/memory/devices \
+	/home/jason/Desktop/ysyx-workbench/npc/wavesim_file/register \
+	/home/jason/Desktop/ysyx-workbench/npc/wavesim_file/sdb \
 
 
 ### Default rules...
@@ -51,5 +73,38 @@ VM_USER_DIR = \
 include Vysyx_23060229_top_classes.mk
 # Include global rules
 include $(VERILATOR_ROOT)/include/verilated.mk
+
+### Executable rules... (from --exe)
+VPATH += $(VM_USER_DIR)
+
+difftest.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/difftest/difftest.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+engine.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/engine/engine.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+gpu.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/memory/devices/gpu.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+keyboard.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/memory/devices/keyboard.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+serial.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/memory/devices/serial.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+timer.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/memory/devices/timer.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+memory.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/memory/memory.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+register.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/register/register.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+expr.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/sdb/expr.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+sdb.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/sdb/sdb.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+watchpoint.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/sdb/watchpoint.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+wavesim.o: /home/jason/Desktop/ysyx-workbench/npc/wavesim_file/wavesim.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+
+### Link rules... (from --exe)
+Vysyx_23060229_top: $(VK_USER_OBJS) $(VK_GLOBAL_OBJS) $(VM_PREFIX)__ALL.a $(VM_HIER_LIBS)
+	$(LINK) $(LDFLAGS) $^ $(LOADLIBES) $(LDLIBS) $(LIBS) $(SC_LIBS) -o $@
+
 
 # Verilated -*- Makefile -*-
