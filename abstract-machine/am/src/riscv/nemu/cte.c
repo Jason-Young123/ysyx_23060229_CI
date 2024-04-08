82f05210e65d9cd24ac8811d1fcd8e69db4abdf9
tracer-ysyx <tracer@ysyx.org>
2024-04-08 12:02:17 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:02:17 up  1:44,  1 user,  load average: 0.00, 0.09, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,6 +7,9 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+	printf("mepc: %x\n", c->mepc);
+	printf("mcause: %x\n", c->mcause);
+	printf("mstatus: %x\n", c-> mstatus);
     switch (c->mcause) {
       default: ev.event = EVENT_YIELD; break;
     }
