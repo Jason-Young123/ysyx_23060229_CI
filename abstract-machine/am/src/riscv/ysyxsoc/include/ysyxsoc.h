1a155f816079cd1b04ad317645c317dcd82e3904
tracer-ysyx <tracer@ysyx.org>
2024-09-29 14:23:24 +0800

    > wavesim in npc ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:23:24 up  2:34,  1 user,  load average: 0.03, 0.08, 0.08

--- abstract-machine/am/src/riscv/ysyxsoc/include/ysyxsoc.h
+++ abstract-machine/am/src/riscv/ysyxsoc/include/ysyxsoc.h
@@ -1,3 +1,11 @@
-#define UART_BASE 0x10000000L
+#define UART_BASE 0x10000000
 #define UART_TX 0
-
+#define UART_LCR 3
+//Line Control Register
+#define UART_DLB1 0
+#define UART_DLB2 1
+//Divisor Latch Byte 1/2
+//假设工作主频为2GHz左右,波特率为115200,则根据计算,Divisor Latch
+//的值应该等于2G/115200/16 ~ 1024,即DL内数据应该为:
+//0000_0100_0000_0000
+//亦即应该向DLB1写入0000_0000,向DLB2写入0000_0100
--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -25,25 +25,35 @@ void putch(char ch){
     *(volatile char *)(UART_BASE + UART_TX) = ch;
 }
 
+void init_uart(){
+	*(volatile char *)(UART_BASE + UART_LCR) = (char)0x83;//1000_0011
+	//*(volatile short *)(UART_BASE + UART_DLB1) = 0x0200;
+	//*(volatile char *)(UART_BASE + UART_DLB1) = (char)'A';//0000_0000
+	*(volatile char *)(UART_BASE + UART_DLB2) = (char)0x02;//0000_0100
+	*(volatile char *)(UART_BASE + UART_DLB1) = (char)0xff;//0000_0000
+	//设置为16'b0000_0100_0000_0000,即1024
+	*(volatile char *)(UART_BASE + UART_LCR) = (char)0x03;//0000_0011,恢复默认值
+}
+
 void halt(int code){
     ysyxsoc_trap(code);
     while(1);
 }
 
 void _trm_init(){
+	
+	//从mrom向sram拷贝数据,模拟bootloader
 	size_t length = (size_t)&_edata_mrom - (size_t)&_sdata_mrom;
 	size_t dest = (size_t)&_sdata_sram;
 	size_t src = (size_t)&_sdata_mrom;
-	//之后将从_data_mrom_start开始长度为length的数据memcpy到_data_sram_start处，用于模拟bootloader
-	//memcpy((void *)dest, (void *)src, length);
     size_t i;
 	for(i = 0; i < length; i++){
-		//*(&_sdata_sram + i) = *(&_sdata_mrom + i);
-		//*(&_sdata_sram + i) = 0;
-		//*((char *)(dest + i)) = 0x34;
 		*((char *)(dest + i)) = *((char *)(src + i));
 	}
 	
+	//初始化串口
+	//init_uart();
+
 	int ret = main(mainargs);
     halt(ret);
 }
