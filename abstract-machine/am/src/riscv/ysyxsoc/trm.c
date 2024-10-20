b6988c35682808e7788cc9c61c854c0046553235
tracer-ysyx <tracer@ysyx.org>
2024-10-20 19:24:01 +0800

    > wavesim in npc ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:01 up  1:00,  1 user,  load average: 0.93, 0.56, 0.30

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,13 +1,14 @@
 #include <am.h>
+#include <klib.h>
 #include <klib-macros.h>
 #include <riscv/riscv.h>
 #include <ysyxsoc.h>
 
 #define ysyxsoc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
-extern char _sdata_mrom;
-extern char _edata_mrom;
-extern char _sdata_sram;
+extern char _sdata_rom;
+extern char _edata_rom;
+extern char _sdata_ram;
 
 extern char _heap_start;
 extern char _heap_end;
@@ -35,6 +36,22 @@ void init_uart(){
 	*(volatile char *)(UART_BASE + UART_DLB1) = (char)0x01;//0000_0000
 	//设置为16'b0000_0100_0000_0000,即1024
 	*(volatile char *)(UART_BASE + UART_LCR) = (char)0x03;//0000_0011,恢复默认值
+	printf("init_uart in trm.c\n");//注意只能在初始化完毕后才能输出信息
+}
+
+void csrr_id(){
+	printf("csrr_id in trm.c\n");
+	unsigned int mvendorid, marchid;
+    asm volatile("csrr %0, mvendorid" : "=r"(mvendorid));
+    asm volatile("csrr %0, marchid" : "=r"(marchid));
+
+    char c0 = (char)(mvendorid & 0x000000ff);
+    char c1 = (char)((mvendorid & 0x0000ff00) >> 8);
+    char c2 = (char)((mvendorid & 0x00ff0000) >> 16);
+    char c3 = (char)((mvendorid & 0xff000000) >> 24);
+    char vendor[5] = {c3, c2, c1, c0, '\0'};
+	//putch(c3); putch(c2); putch(c1); putch(c0); putch('\n');
+    printf("vendorid: %s, archid: %d\n", vendor, marchid);
 }
 
 void halt(int code){
@@ -44,10 +61,10 @@ void halt(int code){
 
 void _trm_init(){
 	
-	//从mrom向sram拷贝数据,模拟bootloader
-	size_t length = (size_t)&_edata_mrom - (size_t)&_sdata_mrom;
-	size_t dest = (size_t)&_sdata_sram;
-	size_t src = (size_t)&_sdata_mrom;
+	//从rom向ram拷贝数据,模拟bootloader
+	size_t length = (size_t)&_edata_rom - (size_t)&_sdata_rom;
+	size_t dest = (size_t)&_sdata_ram;
+	size_t src = (size_t)&_sdata_rom;
     size_t i;
 	for(i = 0; i < length; i++){
 		*((char *)(dest + i)) = *((char *)(src + i));
@@ -55,6 +72,9 @@ void _trm_init(){
 	
 	//初始化串口
 	init_uart();
+
+	//读出ysyx和学号
+	csrr_id();
 
 	int ret = main(mainargs);
     halt(ret);
