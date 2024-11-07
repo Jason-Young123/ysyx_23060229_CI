e2a783f3affb22d86544fc15445c6576783881d1
tracer-ysyx <tracer@ysyx.org>
2024-11-07 12:43:01 +0800

    > wavesim in npc ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:43:01 up  1:55,  1 user,  load average: 0.01, 0.28, 0.38

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/input.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <riscv/riscv.h>
 #include <ysyxsoc.h>
+//#include <klib.h>
 
 #define KEYDOWN_MASK 0x8000
 
@@ -8,6 +9,8 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	uint32_t scancode = inl(KBD_BASE);
     kbd -> keycode = (uint8_t)inb(KBD_BASE);
 	kbd -> keydown = (bool)(scancode >> 8);
+	//if(scancode != 0)
+	//	printf("the scan code is %x\n", scancode);
 	//uint16_t scancode = inw(KBD_BASE);
     //if(scancode & KEYDOWN_MASK){
     //    kbd -> keydown = 1;
