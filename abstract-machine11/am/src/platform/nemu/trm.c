843ea179f964691f0c6d80cc8321067c47cbabf7
tracer-ysyx <tracer@ysyx.org>
2025-03-17 21:13:14 +0800

    > wavesim in npc ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:13 up  6:59,  1 user,  load average: 0.08, 0.02, 0.01

---
+++ abstract-machine11/am/src/platform/nemu/trm.c
@@ -0,0 +1,28 @@
+#include <am.h>
+#include <nemu.h>
+
+extern char _heap_start;
+int main(const char *args);
+
+Area heap = RANGE(&_heap_start, PMEM_END);
+#ifndef MAINARGS
+#define MAINARGS ""
+#endif
+static const char mainargs[] = MAINARGS;
+
+void putch(char ch) {
+  //printf("in am/src/platform/nemu/trm.c");
+  outb(SERIAL_PORT, ch);
+}
+
+void halt(int code) {
+  nemu_trap(code);
+
+  // should not reach here
+  while (1);
+}
+
+void _trm_init() {
+  int ret = main(mainargs);//set the rule that prog starts with main
+  halt(ret);
+}
--- abstract-machine11/scripts/isa/riscv.mk
+++ abstract-machine11/scripts/isa/riscv.mk
@@ -1,7 +1,7 @@
-#CROSS_COMPILE := riscv64-linux-gnu-
-CROSS_COMPILE := riscv32-unknown-elf-
-
+CROSS_COMPILE := riscv64-linux-gnu-
+#CROSS_COMPILE := riscv32-unknown-elf-
 COMMON_CFLAGS := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
+#COMMON_CFLAGS := -fno-pic -mcmodel=medany -mstrict-align
 CFLAGS        += $(COMMON_CFLAGS) -static
 ASFLAGS       += $(COMMON_CFLAGS) -O0
 LDFLAGS       += -melf64lriscv
@@ -9,3 +9,4 @@ LDFLAGS       += -melf64lriscv
 # overwrite ARCH_H defined in $(AM_HOME)/Makefile
 ARCH_H := arch/riscv.h
 
+#$(info #include out1--------------------)
